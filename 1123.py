23e7c6e30517a7f3063976c74c15df697597a14e
HARSIS <91324320+HARSIS@users.noreply.github.com>
2024-02-28 14:17:03 +0300

    Update 1123.py

--- 1123.py
+++ 1123.py
@@ -1,14 +1,25 @@
-def f(a, b):
-    c = ""
-    while (a > 0):
-        c = str(a % b) + c
-        a = a // b
-    return c
-N = 2
-while(N < 8):
-    if (f(4646, N) + f(387, N + 2) == f(3746, N + 1)):
-        print("@@@@@@@@")
-        print(N)
-    N += 1
-print("@@@@@@@@")
-print(N)
+def read_fail(name):
+    """чтение и создание массива
+
+    Ключевые аргументы:
+    name -- название файла
+    """
+    f = open(name).read().split('\n')
+    a = []
+    for i in f:
+        a.append(i.split(','))
+    return a
+
+def search(surname, name, a):
+    """
+    поиск в массиве
+
+    Ключевые аргументы:
+    a -- массив для поиска
+    surname -- фамилия искомого
+    name -- имя искомого
+    """
+
+    b = []
+    for i in 
+print(read_fail("task 14.csv"))
